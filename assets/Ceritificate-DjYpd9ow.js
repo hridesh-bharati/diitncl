caa67a2ce6a972dc87596446acd2f96211bc9f22
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-09 23:16:29 +0000

    deploy: 24e6620f93f048d2c4ec7008b82463a63522a1bb

---
+++ assets/Ceritificate-DjYpd9ow.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-cJJ-1OhA.js";import{C as e}from"./CommanCourse-BRjj_Yp8.js";import"./courseData-CeKRB8nL.js";function i(){const r=["cca","cac"];return t.jsx("div",{className:"MT3",children:t.jsx(e,{targetCourses:r,CTitle:"Computer certificate course"})})}export{i as default};
