ccaf0a5364dc156318c10cb0568058b12b519e29
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-13 16:01:56 +0000

    deploy: a914f3884973fe8639dbb8710f24e281eede65d2

--- assets/Nielet-BzpP7uqA.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BcLEBRJK.js";import{C as o}from"./CommanCourse-Bzln3bSo.js";import"./courseData-CeKRB8nL.js";function C(){const s=["CCC","O LEVEL","A LEVEL","B LEVEL"];return t.jsx("div",{className:"MT3",children:t.jsx(o,{targetCourses:s,CTitle:"NIELIT"})})}export{C as default};
---
+++ assets/OurCourses-BFKTorDR.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D0gKX-V7.js";import{C as s}from"./CommanCourse-D6KRpHgX.js";import"./courseData-CeKRB8nL.js";function m(){return r.jsx("div",{className:"pb-5 pb-lg-0",children:r.jsx(s,{targetCourses:["All"],CTitle:"Computer"})})}export{m as default};
