047cb1adbb79058e0e39bfe91669da29f62b0fb6
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-14 10:22:33 +0000

    deploy: 1cbb8b72ba8d31dd81572f5191c1465c41c082d9

--- assets/Ceritificate-BnNOCC6w.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D0gKX-V7.js";import{C as e}from"./CommanCourse-D6KRpHgX.js";import"./courseData-CeKRB8nL.js";function i(){const r=["cca","cac"];return t.jsx("div",{className:"MT3",children:t.jsx(e,{targetCourses:r,CTitle:"Computer certificate course"})})}export{i as default};
--- assets/Nielet-CRQ_WrLP.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D0gKX-V7.js";import{C as o}from"./CommanCourse-D6KRpHgX.js";import"./courseData-CeKRB8nL.js";function C(){const s=["CCC","O LEVEL","A LEVEL","B LEVEL"];return t.jsx("div",{className:"MT3",children:t.jsx(o,{targetCourses:s,CTitle:"NIELIT"})})}export{C as default};
