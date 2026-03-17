9ee81320ba15a3566e1364396fee049988b3d287
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-17 13:26:05 +0000

    deploy: 84c2a9faca7be89620cf6910ee5fe9a72f5db3f7

---
+++ assets/Banking-Bu6eLt-4.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Cugp0svB.js";import{C as o}from"./CommanCourse-CCyLO3YM.js";import"./courseData-CeKRB8nL.js";function a(){const s=["DBI"];return t.jsx("div",{className:"MT3",children:t.jsx(o,{targetCourses:s,CTitle:"Banking"})})}export{a as default};
---
+++ assets/OurCourses-Capk54wk.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Cugp0svB.js";import{C as s}from"./CommanCourse-CCyLO3YM.js";import"./courseData-CeKRB8nL.js";function m(){return r.jsx("div",{className:"pb-5 pb-lg-0",children:r.jsx(s,{targetCourses:["All"],CTitle:"Computer"})})}export{m as default};
