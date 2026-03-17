9426eb9e9055d6e2af3c5eadf5ecbaa2c40eb189
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-17 21:24:52 +0000

    deploy: 396738cda3d398d66aae2ad377049c4f527d180f

---
+++ assets/Banking-Bj0-3iAT.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Cd-fi2Gc.js";import{C as o}from"./CommanCourse-BG2mZtOL.js";import"./courseData-CeKRB8nL.js";function a(){const s=["DBI"];return t.jsx("div",{className:"MT3",children:t.jsx(o,{targetCourses:s,CTitle:"Banking"})})}export{a as default};
