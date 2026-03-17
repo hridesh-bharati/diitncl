ac48f77de749326f6fbe4f61cf7aebbd009e4611
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-17 13:20:38 +0000

    deploy: e148c24f5b21a6b246a002df8f78aa39a4bb9ae9

--- assets/Banking-SOIr_zc2.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DG1GR7jY.js";import{C as o}from"./CommanCourse-Df-Fm8a-.js";import"./courseData-CeKRB8nL.js";function a(){const s=["DBI"];return t.jsx("div",{className:"MT3",children:t.jsx(o,{targetCourses:s,CTitle:"Banking"})})}export{a as default};
---
+++ assets/Designing-krwY62fg.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DRDKazBl.js";import{C as e}from"./CommanCourse-y-osvFNb.js";import"./courseData-CeKRB8nL.js";function m(){const t=["DTP"];return s.jsx("div",{className:"MT3",children:s.jsx(e,{targetCourses:t,CTitle:"Desingn"})})}export{m as default};
