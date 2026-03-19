d33e4d03fc960baef4262686a72a00f5290cf07e
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-19 10:29:42 +0000

    deploy: bd28075dd78ab7b7e562157ac2e619360adf9025

--- assets/Nielet-MCuhNhqC.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-P8UQ7HK-.js";import{C as o}from"./CommanCourse-C8sEy8Y9.js";import"./courseData-CeKRB8nL.js";function C(){const s=["CCC","O LEVEL","A LEVEL","B LEVEL"];return t.jsx("div",{className:"MT3",children:t.jsx(o,{targetCourses:s,CTitle:"NIELIT"})})}export{C as default};
