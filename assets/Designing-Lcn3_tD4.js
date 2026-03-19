b74e7a12732e828090f892e24b7a6f50795155a6
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-19 07:32:09 +0000

    deploy: 49bcc966c04c9071dc62b0dd7d4417f8de8f1fb9

--- assets/Designing-Lcn3_tD4.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-cMHkz8K0.js";import{C as e}from"./CommanCourse-Duw6gmF8.js";import"./courseData-CeKRB8nL.js";function m(){const t=["DTP"];return s.jsx("div",{className:"MT3",children:s.jsx(e,{targetCourses:t,CTitle:"Desingn"})})}export{m as default};
--- assets/OurCourses-BQPhyS71.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-cMHkz8K0.js";import{C as s}from"./CommanCourse-Duw6gmF8.js";import"./courseData-CeKRB8nL.js";function m(){return r.jsx("div",{className:"pb-5 pb-lg-0",children:r.jsx(s,{targetCourses:["All"],CTitle:"Computer"})})}export{m as default};
