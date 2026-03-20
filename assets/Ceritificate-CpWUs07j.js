6a5944a269cb4ec5801bd2bda133d482e05ffbdc
hridesh-bharati <hridesh-bharati@users.noreply.github.com>
2026-03-20 07:00:51 +0000

    deploy: 4545c91fa126506395785471ac53fa8799851b4c

---
+++ assets/Ceritificate-CpWUs07j.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Y_RrK5XW.js";import{C as e}from"./CommanCourse-knEpJE-S.js";import"./courseData-CeKRB8nL.js";function i(){const r=["cca","cac"];return t.jsx("div",{className:"MT3",children:t.jsx(e,{targetCourses:r,CTitle:"Computer certificate course"})})}export{i as default};
---
+++ assets/OurCourses-DzOiy7AH.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Y_RrK5XW.js";import{C as s}from"./CommanCourse-knEpJE-S.js";import"./courseData-CeKRB8nL.js";function m(){return r.jsx("div",{className:"pb-5 pb-lg-0",children:r.jsx(s,{targetCourses:["All"],CTitle:"Computer"})})}export{m as default};
