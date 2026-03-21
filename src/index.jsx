e65361a950bc1ea7c46986c32271e1a65300606d
Hridesh Bharati <hridesh027@gmail.com>
2026-03-21 08:51:33 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from "react-toastify";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App";
